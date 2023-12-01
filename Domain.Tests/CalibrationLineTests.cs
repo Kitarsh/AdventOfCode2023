6415c2374a18a2bbaef212474aacc497c04f0fd6
Kitarsh <thomas.leclere@centrale-marseille.fr>
2023-12-01 23:46:25 +0100

    Second step but mismatching for last element

--- Domain.Tests/CalibrationLineTests.cs
+++ Domain.Tests/CalibrationLineTests.cs
@@ -16,10 +16,34 @@ namespace Domain.Tests
         }
 
         [TestMethod]
-        public void ComputeLinesOfFile()
+        public void GetOneAsDigit()
+        {
+            var input = "aaaaoneaaaaa2";
+            var expectedResult = 12;
+
+            var line = new CalibrationLine(input);
+            int lineNumber = line.GetNumber();
+
+            Assert.AreEqual(expectedResult, lineNumber);
+        }
+
+        [TestMethod]
+        public void ComputeLinesOfFileStep1()
         {
             var inputFile = "SampleTest.txt";
             var expectedResult = 142;
+
+            var calibration = new Calibration(inputFile);
+            int result = calibration.GetTotal();
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void ComputeLinesOfFileStep2()
+        {
+            var inputFile = "SecondSampleText.txt";
+            var expectedResult = 281;
 
             var calibration = new Calibration(inputFile);
             int result = calibration.GetTotal();
--- Domain/Models/CalibrationLine.cs
+++ Domain/Models/CalibrationLine.cs
@@ -4,6 +4,8 @@ namespace Domain;
 
 public class CalibrationLine
 {
+    private const string PatternWithLetters = "[1-9]|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)";
+    private const string PatternWithoutLetters = "[1-9]";
     private string input;
 
     public CalibrationLine(string input)
@@ -13,13 +15,38 @@ public class CalibrationLine
 
     public int GetNumber()
     {
-        var regex = new Regex("[1-9]");
+        var regex = new Regex(PatternWithLetters);
         var result = regex.Matches(this.input);
 
-        var firstChar = result.First().Value.ToCharArray().First();
-        var lastChar = result.Last().Value.ToCharArray().Last();
-
+        char firstChar = GetFirstNumber(result);
+        char lastChar = GetLastNumber(result);
+        Console.WriteLine($"{firstChar}{lastChar} {this.input}");
         var numberStr = new string((new char[] { firstChar, lastChar }));
         return Int32.Parse(numberStr);
+    }
+
+    private static char GetLastNumber(MatchCollection result)
+        => TransformToNumber(result.Last().Value);
+
+    private static char GetFirstNumber(MatchCollection result)
+        => TransformToNumber(result.First().Value);
+
+    private static char TransformToNumber(string value)
+    {
+        if (value.Length == 1) return value[0];
+
+        switch (value)
+        {
+            case "one": return '1';
+            case "two": return '2';
+            case "three": return '3';
+            case "four": return '4';
+            case "five": return '5';
+            case "six": return '6';
+            case "seven": return '7';
+            case "eight": return '8';
+            case "nine": return '9';
+        }
+        throw new ApplicationException("CannotTransformNumber");
     }
 }
